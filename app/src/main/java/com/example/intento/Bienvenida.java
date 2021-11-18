c18bf049743e3f6c22806d6554b42ea17f238304
FlorezJulian <florezjulian87@gmail.com>
2021-11-17 20:59:44 -0500

    Creaciónn
    Creacin

---
+++ app/src/main/java/com/example/intento/Bienvenida.java
@@ -0,0 +1,59 @@
+package com.example.intento;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.Button;
+
+import com.example.intento.databinding.ActivityBienvenidaBinding;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Bienvenida extends AppCompatActivity {
+
+    Button login, registro;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+
+        this.login= findViewById(R.id.login_boton);
+        this.registro=findViewById(R.id.registro_boton);
+        this.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new  Intent(Bienvenida.this,Login.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        this.registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new  Intent(Bienvenida.this,Registro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/intento/Registro.java
@@ -0,0 +1,36 @@
+package com.example.intento;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+
+import com.example.intento.databinding.ActivityRegistroBinding;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Registro extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_registro);
+
+    }
+
+
+
+
+}
